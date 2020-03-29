61eb2df081e62f883ed18ae6c1edbee02621e6af
baixiao <baixiaohub@gmail.com>
2020-03-29 19:03:01 +0800

    book1, ch.6 add a sphere.

--- RTOneWeekend/main.cpp
+++ RTOneWeekend/main.cpp
@@ -7,44 +7,85 @@
 
 #include <vector>
 
+bool hit_sphere(const vec3& center, real radius, const ray& r)
+{
+	vec3 oc = r.origin - center;
+	real a = dot(r.dir, r.dir);
+	real b = 2.0f * dot(oc, r.dir);
+	real c = dot(oc, oc) - radius * radius;
+	real discriminant = b * b - 4 * a * c;
+
+	return discriminant > 0;
+}
+
+vec3 ray_color(const ray& r)
+{
+	if (hit_sphere(vec3(0, 0, -1.0f), 0.5f, r))
+	{
+		return vec3(1.0f, 0.0f, 0.0f);
+	}
+
+	vec3 unit_dir = (r.dir);
+	real t = 0.5f * (unit_dir.y + 1.0f);
+	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+}
+
+void write_color_rgba(int32_t x, int32_t y, uint32_t image_width, uint32_t image_height,
+	bool flipY, const vec3& color, uint8_t* data)
+{
+	const uint32_t IMAGE_CHANNELS = 4;
+	uint8_t ir = (uint8_t)(255.999f * color.x);
+	uint8_t ig = (uint8_t)(255.999f * color.y);
+	uint8_t ib = (uint8_t)(255.999f * color.z);
+
+	uint64_t offset = 0;
+	if (flipY) {
+		offset = ((image_height - y - 1) * image_width + x)* IMAGE_CHANNELS;
+	}
+	else {
+		offset = (y * image_width + x)* IMAGE_CHANNELS;
+	}
+	data[offset] = ir;//r
+	data[offset + 1] = ig;//g
+	data[offset + 2] = ib;//b
+	data[offset + 3] = 255; //a
+}
 
 int main()
 {
-	const uint32_t IMAGE_WIDTH = 800;
-	const uint32_t IMAGE_HEIGHT = 600;
+	const uint32_t IMAGE_WIDTH = 400;
+	const uint32_t IMAGE_HEIGHT = 200;
 	const uint32_t IMAGE_CHANNELS = 4;
 	const uint32_t IMAGE_PIXEL_BYTES = IMAGE_WIDTH * IMAGE_HEIGHT * IMAGE_CHANNELS;
 	const char* filename = "image.png";
 
-	std::vector<uint8_t> data(IMAGE_PIXEL_BYTES);
-	for (uint32_t y = 0; y < IMAGE_HEIGHT; ++y)
+	vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
+	vec3 horizontal(4.0f, 0.0f, 0.0f);
+	vec3 vertical(0.0f, 2.0f, 0.0f);
+	vec3 origin(0.0f, 0.0f, 0.0f);
+
+	std::vector<uint8_t> buffer(IMAGE_PIXEL_BYTES);
+	for (int32_t y = 0; y < IMAGE_HEIGHT; ++y)
 	{
 		fprintf(stderr, "Scanlines remaining: %u\n", y);
-		for (uint32_t x = 0; x < IMAGE_WIDTH; ++x)
+		for (int32_t x = 0; x < IMAGE_WIDTH; ++x)
 		{
-			uint64_t offset = (y * IMAGE_WIDTH + x) * IMAGE_CHANNELS;
+			real u = 1.0f * x / IMAGE_WIDTH;
+			real v = 1.0f * y / IMAGE_HEIGHT;
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
 
-			float r = 1.0f * x / IMAGE_WIDTH;
-			float g = 1.0f * y / IMAGE_HEIGHT;
-			float b = 0.2f;
+			vec3 color = ray_color(r);
+			//color = vec3_pow(color, 2.2f);
 
-			vec3 color(r, g, b);
-			color = vec3_pow(color, 2.2f);
-
-			uint8_t ir = (uint8_t)(255.999f * color.x);
-			uint8_t ig = (uint8_t)(255.999f * color.y);
-			uint8_t ib = (uint8_t)(255.999f * color.z);
-
-			data[offset] = ir;//r
-			data[offset + 1] = ig;//g
-			data[offset + 2] = ib;//b
-			data[offset + 3] = 255; //a
+			write_color_rgba(x, y, IMAGE_WIDTH, IMAGE_HEIGHT, true, color, buffer.data());
 		}
 	}
 
-	bool succeed = image_io_write_png_file(filename, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS, IMAGE_WIDTH * IMAGE_CHANNELS, data.data());
-
+	bool succeed = image_io_write_png_file(filename, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS, IMAGE_WIDTH * IMAGE_CHANNELS, buffer.data());
 	assert(succeed && "write png file failed!");
+
+	fprintf(stderr, "Done.\n");
+	system(filename);
 
 	return 0;
 }
--- RTOneWeekend/math_vec.h
+++ RTOneWeekend/math_vec.h
@@ -27,11 +27,25 @@ inline vec3 operator/(const vec3& a, const vec3& b) { return vec3(a.x / b.x, a.y
 inline vec3 operator*(const vec3& a, real f) { return vec3(f * a.x, f * a.y, f * a.z); }
 inline vec3 operator*(real f, const vec3& a) { return vec3(f * a.x, f * a.y, f * a.z); }
 inline vec3 operator/(const vec3& a, real f) { return vec3(a.x / f, a.y / f, a.z / f); }
+inline vec3 operator-(const vec3& a) { return vec3(-a.x, -a.y, -a.z); }
 
 inline real dot(const vec3& a, const vec3& b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
 inline real length_squared(const vec3& a) { return dot(a, a); }
 inline real length(const vec3& a) { return sqrt(dot(a, a)); }
 inline vec3 normalize(const vec3& a) { return a / length(a); }
 
+inline vec3 cross(const vec3& a, const vec3& b) { return vec3(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y + a.y*b.x); }
 inline vec3 vec3_pow(const vec3& a, real p) { return vec3(pow(a.x, p), pow(a.y, p), pow(a.z, p)); }
+
+
+struct ray
+{
+	ray() {}
+	ray(const vec3& origin, const vec3& dir) :origin(origin), dir(dir) {}
+
+	vec3 origin;
+	vec3 dir;
+};
+
+inline vec3 ray_at(const ray& r, real t) { return r.origin + t * r.dir; }
 
